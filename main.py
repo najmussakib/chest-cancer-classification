2665fae111478e08a0726ec4077c28e75489824e
Najmus Saqib <n.saqib@almaviva.it>
2025-04-17 02:37:44 +0200

    add training pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chest_classifier import logger
 from chest_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chest_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chest_classifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -19,6 +20,18 @@ try:
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = ModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
